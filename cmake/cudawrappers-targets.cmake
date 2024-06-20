609cde6ed85c1ab788eaabec758ffbf85fec17e1
Leon Oostrum <l.oostrum@esciencecenter.nl>
2024-06-20 10:08:53 +0200

    Add macros as target so the header is installed

--- cmake/cudawrappers-targets.cmake
+++ cmake/cudawrappers-targets.cmake
@@ -4,11 +4,12 @@ get_filename_component(
 )
 
 # Define all the individual components that cudawrappers provides
-set(CUDAWRAPPERS_COMPONENTS cu cufft nvml nvrtc nvtx)
+set(CUDAWRAPPERS_COMPONENTS cu cufft nvml nvrtc nvtx macros)
 # set(LINK_cu CUDA::cuda_driver) set(LINK_cufft CUDA::cuda_driver CUDA::cufft)
 # set(LINK_nvml CUDA::cuda_driver CUDA::nvml) set(LINK_nvrtc CUDA::cuda_driver
 # CUDA::nvrtc) set(LINK_nvtx CUDA::nvToolsExt)
 
+set(LINK_macros hip::host)
 set(LINK_cu hip::host)
 set(LINK_cufft hip::host hip::hipfft)
 set(LINK_nvml hip::host)
